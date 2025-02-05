fd7abcbc110563257454e4cb70ea6b618bacdb2d
Anshul Kumar <f20230621@goa.bits-pilani.ac.in>
2025-02-05 17:56:32 +0530

    bc

---
+++ README.md
@@ -0,0 +1,18 @@
+# Delete the existing recordings
+assignment_recording.txt ke andar ke contents ko delete kar dena
+
+# Run the following command
+**Ek time pe ek command run karna**
+
+```
+script rec.txt
+./assign1
+exit
+```
+# Zip file Banane ke liye
+```
+zip <roll_no>_assignment_1.zip assign1.c rec.txt
+```
+`Eg. zip 2023aaps0621g_assignment_1.zip assign1.c rec.txt`
+
+# Directly bas zip file sumbit kar dena bkl
--- assign1.c
+++ assign1.c
@@ -8,45 +8,36 @@ int main() {
     pid_t pid1, pid2;
     int status;
 
-    // Print parent process information
     printf("Process PID: %d, Role: Parent\n", getpid());
 
-    // Create first child
     pid1 = fork();
 
     if (pid1 < 0) {
-        // Fork failed
         fprintf(stderr, "Fork Failed\n");
         return 1;
     }
     else if (pid1 == 0) {
-        // First child process
         printf("Process PID: %d, Role: Child 1\n", getpid());
         exit(0);
     }
     else {
-        // Parent process creates second child
         pid2 = fork();
 
         if (pid2 < 0) {
-            // Fork failed
             fprintf(stderr, "Fork Failed\n");
             return 1;
         }
         else if (pid2 == 0) {
-            // Second child process
             printf("Process PID: %d, Role: Child 2\n", getpid());
             exit(0);
         }
         else {
-            // Parent process waits for both children
             waitpid(pid1, &status, 0);
             printf("Child with PID: %d, Role: Child 1 has exited\n", pid1);
             
             waitpid(pid2, &status, 0);
             printf("Child with PID: %d, Role: Child 2 has exited\n", pid2);
 
-            // Parent's final message before exit
             printf("Process PID: %d, Role: Parent - Exiting\n", getpid());
             exit(0);
         }
